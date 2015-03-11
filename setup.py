bfd109ffae243b330c7d0f8f3167d2a828925a83
Okami <okami@fuzetsu.info>
2015-03-11 18:55:33 +0300

    version updated

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 data = {
     'name': 'djradicale',
-    'version': '0.0.2',
+    'version': '0.0.3',
     'author': 'Okami',
     'author_email': 'okami@fuzetsu.info',
     'description': (
