e68919f82d261ec12a6c52cacbed428b3b137597
Okami <okami@fuzetsu.info>
2016-06-06 13:36:06 +0300

    initial migrations

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 data = {
     'name': 'djradicale',
-    'version': '0.0.4',
+    'version': '0.0.5',
     'author': 'Okami',
     'author_email': 'okami@fuzetsu.info',
     'description': (
